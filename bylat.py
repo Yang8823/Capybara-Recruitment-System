302d7a3e9b2283d6321fa391abfff5bef5fd8fb8
Eajun Ooi <eajunooi@gmail.com>
2023-10-18 17:12:12 +0800

    commit3

--- bylat.py
+++ bylat.py
@@ -4,15 +4,19 @@ import cv2
 import numpy as np
 
 def process_cv_image(image_path):
-    image = cv2.imread(image_path)                                                      # Load the image using OpenCV
-    
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)                                      # Preprocess the image (grayscale, filtering, binarization)
+    # Load the image using OpenCV
+    image = cv2.imread(image_path)
+
+    # Preprocess the image: convert to grayscale, apply adaptive thresholding
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (5, 5), 0)
-    _, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    binary = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, 10)
 
-    extracted_text = pytesseract.image_to_string(Image.fromarray(binary))               # Perform OCR using Tesseract
+    # Perform OCR using Tesseract
+    custom_config = r'--oem 3 --psm 6'
+    extracted_text = pytesseract.image_to_string(Image.fromarray(binary), config=custom_config)
 
-                                                                                        # Further data extraction and analysis can be done here, e.g: parsing contact information, work experience, education, etc.
+    # Further data extraction and analysis can be done here
 
     return extracted_text
 
@@ -23,5 +27,5 @@ print(cv_text)
 specific_text = "John Meehan"
 if specific_text in cv_text:
     print(f"Found: {specific_text}")
-else:                                                                                   #optional
+else:
     print(f"Not found: {specific_text}")
